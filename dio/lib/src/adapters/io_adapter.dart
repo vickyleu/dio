4b5b5ded207f40a94561ffacbebb8c66208751b8
vickyleu <yu6564172@gmail.com>
2020-09-28 18:53:28 +0800

    header lowercase fix

--- dio/lib/src/adapters/io_adapter.dart
+++ dio/lib/src/adapters/io_adapter.dart
@@ -46,7 +46,7 @@ class DefaultHttpClientAdapter implements HttpClientAdapter {
     try {
       request = await requestFuture;
       //Set Headers
-      options.headers.forEach((k, v) => request.headers.set(k, v));
+      options.headers.forEach((k, v) => request.headers.set(k, v ,preserveHeaderCase:true));
     } on SocketException catch (e) {
       if (e.message.contains('timed out')) _throwConnectingTimeout();
       rethrow;
